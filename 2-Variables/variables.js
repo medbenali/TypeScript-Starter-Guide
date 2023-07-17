e4f9dea949ccb97671ec3bb169d7c79c2052e731
Mohamed BEN ALI <mohamed.benali@esprit.tn>
2023-07-17 14:03:17 +0100

    Added Typescript Variables Code

---
+++ 2-Variables/variables.js
@@ -0,0 +1,17 @@
+var found = true;
+var grade = 96.5;
+var firstName = "Mohamed";
+var lastName = 'BEN ALI';
+// let's break it!
+/*
+
+found = 0;
+grade = "A";
+firstName = false;
+
+*/
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template Strings
+console.log("Hi ".concat(firstName, " ").concat(lastName));
