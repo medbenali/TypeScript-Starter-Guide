fba1c3e2ec053c0b5fef09e26d584fa29e7f6907
Mohamed BEN ALI <mohamed.benali@esprit.tn>
2023-07-19 12:27:24 +0100

    Added Typescript Inheritance Code

---
+++ 5-Inheritance/Circle.ts
@@ -0,0 +1,26 @@
+import { Shape } from "./Shape";
+
+export class Circle extends Shape
+{
+    private _radius: number;
+   
+
+    constructor(theX: number, theY: number, radius: number)
+    {
+        super(theX, theY);
+        this._radius = radius;
+    }
+
+    public get radius(): number {
+        return this._radius;
+    }
+
+    public set radius(value: number) {
+        this._radius = value;
+    }
+    
+    getInfo(): string
+    {
+        return super.getInfo() + `, radius=${this._radius}`;
+    }
+}
---
+++ 5-Inheritance/Rectangle.ts
@@ -0,0 +1,38 @@
+import { Shape } from './Shape';
+
+export class Rectangle extends Shape
+{
+    private _width: number;
+    private _length: number;
+    
+
+    constructor(theX: number, theY: number, width: number, length: number)
+    {
+        super(theX, theY);
+        this._width = width;
+        this._length = length;
+    }
+
+    public get width(): number {
+        return this._width;
+    }
+
+    public set width(value: number) {
+        this._width = value;
+    }
+
+    public get length(): number {
+        return this._length;
+    }
+
+    public set length(value: number) {
+        this._length = value;
+    }
+
+    getInfo(): string
+    {
+        return super.getInfo() + `, width=${this._width}, length=${this._length}`;
+    }
+
+
+}
