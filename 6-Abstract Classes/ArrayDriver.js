cc9a25178da6119621a583efcc4f6eeca0e041fc
Mohamed BEN ALI <mohamed.benali@esprit.tn>
2023-07-23 09:42:54 +0100

    Added Typescript Abstract Classes Code

---
+++ 6-Abstract Classes/ArrayDriver.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
+var myCircle = new Circle_1.Circle(5, 10, 20);
+var myRectangle = new Rectangle_1.Rectangle(0, 0, 3, 7);
+//console.log(myCircle.calculateArea());
+//console.log(myRectangle.calculateArea());
+// declare an array of shapes ... initially empty
+var theShapes = [];
+// add the shapes to the array
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+//theShapes.push(new Shape(30, 45));
+for (var _i = 0, theShapes_1 = theShapes; _i < theShapes_1.length; _i++) {
+    var tempShape = theShapes_1[_i];
+    console.log(tempShape.getInfo());
+    console.log(tempShape.calculateArea());
+    console.log();
+    //console.log("\n");
+}
